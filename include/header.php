53c1bac1118d1f786919160f9f9165451bd706c8
Umar Farooq <mis2@alshahinmail.com>
2025-09-09 10:51:32 +0300

    Removing extra comments

--- include/header.php
+++ include/header.php
@@ -37,12 +37,6 @@
 		<link type="text/css" rel="stylesheet" href="css/style.css"/>	
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
  
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
 
     </head>
 	<style>
